f347d98693fa091b1e20ecb3b9cd3c6080ed907f
Marcin Dziewulski <hello@mobily.pl>
2021-07-07 15:22:36 +0200

    Update forkJoin types

--- src/WonkaExtras_types.ts
+++ src/WonkaExtras_types.ts
@@ -52,6 +52,72 @@ export declare type sources = {
       sourceT<J>,
     ],
   ): sourceT<[A, B, C, D, E, F, G, H, I, J]>
+  <A, B, C, D, E, F, G, H, I, J, K>(
+    sources: [
+      sourceT<A>,
+      sourceT<B>,
+      sourceT<C>,
+      sourceT<D>,
+      sourceT<E>,
+      sourceT<F>,
+      sourceT<G>,
+      sourceT<H>,
+      sourceT<I>,
+      sourceT<J>,
+      sourceT<K>,
+    ],
+  ): sourceT<[A, B, C, D, E, F, G, H, I, J, K]>
+  <A, B, C, D, E, F, G, H, I, J, K, L>(
+    sources: [
+      sourceT<A>,
+      sourceT<B>,
+      sourceT<C>,
+      sourceT<D>,
+      sourceT<E>,
+      sourceT<F>,
+      sourceT<G>,
+      sourceT<H>,
+      sourceT<I>,
+      sourceT<J>,
+      sourceT<K>,
+      sourceT<L>,
+    ],
+  ): sourceT<[A, B, C, D, E, F, G, H, I, J, K, L]>
+  <A, B, C, D, E, F, G, H, I, J, K, L, M>(
+    sources: [
+      sourceT<A>,
+      sourceT<B>,
+      sourceT<C>,
+      sourceT<D>,
+      sourceT<E>,
+      sourceT<F>,
+      sourceT<G>,
+      sourceT<H>,
+      sourceT<I>,
+      sourceT<J>,
+      sourceT<K>,
+      sourceT<L>,
+      sourceT<M>,
+    ],
+  ): sourceT<[A, B, C, D, E, F, G, H, I, J, K, L, M]>
+  <A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
+    sources: [
+      sourceT<A>,
+      sourceT<B>,
+      sourceT<C>,
+      sourceT<D>,
+      sourceT<E>,
+      sourceT<F>,
+      sourceT<G>,
+      sourceT<H>,
+      sourceT<I>,
+      sourceT<J>,
+      sourceT<K>,
+      sourceT<L>,
+      sourceT<M>,
+      sourceT<N>,
+    ],
+  ): sourceT<[A, B, C, D, E, F, G, H, I, J, K, L, M, N]>
 }
 
 export declare const forkJoin: sources
